081d0e6a32abf7995cfe7de4d32696ce69974415
Lorenza Pasquini <lorenza.pasquini@hotmail.it>
2017-06-05 23:11:32 +0200

    Aggiornamento del file .sql
    Creazione e popolazione della tabella Utente

--- includes/hair-future.sql
+++ includes/hair-future.sql
@@ -37,6 +37,18 @@ CREATE TABLE `Appuntamento` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Table structure for table `Utente`
+--
+
+CREATE TABLE `Utente` (
+ `nome` char(20) NOT NULL,
+ `cognome` char(20) NOT NULL,
+ `recapito` int(10) NOT NULL,
+ `email` char(20) NOT NULL,
+ `password` char(20) NOT NULL,
+ `tipo` char(20) NOT NULL);
+
+--
 -- Dumping data for table `Appuntamento`
 --
 
@@ -44,6 +56,16 @@ INSERT INTO `Appuntamento` (`codice`, `data`, `ora`, `durata`, `costo`, `utente`
 (1, '2017-05-08', '09:00:00', 120, 0, 1, '0001,0002,0005'),
 (2, '2017-05-09', '11:00:00', 60, 0, 2, '0001,0005'),
 (3, '2017-05-08', '14:00:00', 90, 0, 1, '0001,0003');
+
+
+--
+-- Dumping data for table `Utente`
+--
+
+INSERT INTO `Utente` (`nome`, `cognome`, `recapito`, `email`, `password`, `tipo`) VALUES
+('Carlo', 'Attardi', '3896666666', 'example1@hotmail.it', 'password1', 'parrucchiere' ),
+('Marco', 'Stigliano', '3896666667', 'example2@hotmail.it', 'password2', 'parrucchiere'),
+('Giuseppe Pio', 'Carlone', '3896666668', 'example3@hotmail.it', 'password3', 'parrucchiere');
 
 --
 -- Indexes for dumped tables
